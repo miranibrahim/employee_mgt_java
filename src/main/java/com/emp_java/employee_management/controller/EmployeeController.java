5bd8bf5760e482c281289c88ecd6138f9f69f860
miranibrahim <moeenuddin.miran@gmail.com>
2026-08-20 23:13:08 +0600

    commit structure 2

--- src/main/java/com/emp_java/employee_management/controller/EmployeeController.java
+++ src/main/java/com/emp_java/employee_management/controller/EmployeeController.java
@@ -4,19 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.emp_java.employee_management.dto.request.CreateEmployeeRequest;
+import com.emp_java.employee_management.dto.request.UpdateEmployeeRequest;
 import com.emp_java.employee_management.dto.response.ResponseEmployee;
 import com.emp_java.employee_management.service.EmployeeService;
 
 import jakarta.validation.Valid;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 
 @RestController
 @RequestMapping("/api/v1/employees")
@@ -35,10 +34,11 @@ public class EmployeeController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<ResponseEmployee> updateEmployee(@Valid @RequestBody @CreateEmployeeRequest request) {
-        
-        // ResponseEmployee response = employeeService.
-        
+    public ResponseEntity<ResponseEmployee> updateEmployee(@PathVariable Long id,
+            @Valid @RequestBody UpdateEmployeeRequest request) {
+
+        ResponseEmployee response = employeeService.updateEmployee(id, request);
+
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
